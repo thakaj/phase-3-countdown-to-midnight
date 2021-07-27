5eddc56c6299326f5730b486a0993f4f0eaf40aa
thakaj <taulant.hakaj@scranton.edu>
2021-07-27 16:30:43 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
 
-def countdown
+def countdown(integer)
+    while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countdown)
+    sleep(5)
 end
